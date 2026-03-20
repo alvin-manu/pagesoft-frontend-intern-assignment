bec9efc57843269f5b5feea5f802b826ea1d860c
Alvin00111 <wmawma905@gmail.com>
2026-03-20 12:07:52 +0530

    added digital clock

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const darkModeToggle = document.getElementById("darkModeToggle")
 const menuToggle = document.getElementById("menuToggle")
 const navLists = document.getElementById("navLists")
 const menuToggleIcon = document.getElementById("menuToggleIcon")
+const hours = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+const timeSection = document.getElementById("timeSection")
 
 darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle("dark");
@@ -17,5 +21,18 @@ darkModeToggle.addEventListener('click', () => {
 
 menuToggle.addEventListener("click", () => {
     navLists.classList.toggle("active")
-    
 })
+
+setInterval(() => {
+    const date = new Date();
+    const hours = formatTime(date.getHours())
+    const minutes = formatTime(date.getMinutes())
+    const seconds = formatTime(date.getSeconds())
+    const ampm = hours>=12 ? "PM": "AM"
+    timeSection.innerHTML = `${hours} : ${minutes} : ${seconds} ${ampm}`
+}, 1000)
+
+const formatTime = (val) => {
+    const time = val < 10 ? '0' + val : val
+    return time
+}
